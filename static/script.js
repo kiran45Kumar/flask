b8ea58664f8e3d05b0d2a8934a2deb00032af63b
kiran45Kumar <161808915+kiran45Kumar@users.noreply.github.com>
2024-07-13 16:01:41 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,21 @@
+
+let price = document.getElementById('pricing-tag');
+let price2 = document.getElementById('pricing-tag2');
+let price3 = document.getElementById('pricing-tag3');
+price.classList.add('lead');
+price2.classList.add('lead');
+price3.classList.add('lead');
+
+
+function servicesPage(){
+   window.location.href = '/services';
+}
+function contactPage(){
+   window.location.href = '/register';
+}
+function pricingPage(){
+   window.location.href = '/pricing';
+}
+function aboutusPage(){
+   window.location.href = '/about';
+}
