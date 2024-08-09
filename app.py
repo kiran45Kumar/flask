e09d3f7f224c4d40c402c09dd334744db516fc47
kiran45Kumar <161808915+kiran45Kumar@users.noreply.github.com>
2024-08-09 08:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, redirect, render_template, request, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+import psycopg2
 import re
 from datetime import datetime
-
-
 app = Flask(__name__)
 app.secret_key = 'Kiran5678'
 
-app.config['MYSQL_HOST'] = 'ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'p85uwbv2uoatle8h'
-app.config['MYSQL_PASSWORD'] = 'c3ho6v9ad7xqx746'
-app.config['MYSQL_DB'] = 'dubw4vzh6016qefd'
-app.config['MYSQL_PORT'] = 3306
+DB_HOST = 'ep-dark-bush-a17psoow.ap-southeast-1.pg.koyeb.app'
+DB_PORT = '5432'
+DB_NAME = 'koyebdb'
+DB_USER = 'koyeb-adm'
+DB_PASSWORD = 'lnrUth6pH0JS'
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    return conn
 
-mysql = MySQL(app)
 @app.route('/')
 def main():
     name = 'Jussure Technologies'
@@ -27,7 +32,7 @@ def services():
 def about():
     title = "About Us"
     return render_template('aboutus.html',title = title)
-@app.route('/register',methods = ['POST','GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     msg = ''
     if request.method == 'POST':
@@ -36,23 +41,33 @@ def register():
         number = request.form['number']
         address = request.form['address']
         message = request.form['message']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM accounts WHERE name = %s', (username,))
         account = cursor.fetchone()
-        if not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not username or not number or not email or not address or not message:
-            msg = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s ,% s, % s)', (username, email, number, address, message))
-            mysql.connection.commit()
-            msg = user(username)
-    elif request.method == 'POST':
-        msg = 'Please fill out the form !'
 
-    return render_template('contactus.html',msg = msg,)
+        if not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not username or not number or not email or not address or not message:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute(
+                'INSERT INTO accounts (name, gmail, number, address, message) VALUES (%s, %s, %s, %s, %s)',
+                (username, email, number, address, message)
+            )
+            conn.commit()
+            msg = user(username)
+        
+        cursor.close()
+        conn.close()
+
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+
+    return render_template('contactus.html', msg=msg)
 @app.route('/pricing')
 def pricing():
     title = "Pricing"
@@ -70,4 +85,4 @@ def thanks():
     tmsg = 'Thanks your form has been submitted'
     return render_template('thanks.html',thmsg = tmsg)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="localhost")
